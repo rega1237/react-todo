61869e12ff6d434a0b01563bcee27aac3ad76c8c
Rafael Guzman <rega1237@gmail.com>
2022-05-31 17:51:40 -0400

    todo app

---
+++ src/classBased/components/TodosList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+class TodosList extends React.Component {
+  render() {
+    return (      
+    <React.Fragment>
+      <ul>
+        {this.props.todos.map(todo => (
+        <TodoItem 
+        key={todo.id} 
+        todo={todo} 
+        handleChangeProps={this.props.handleChangeProps} 
+        deleteTodoProps={this.props.deleteTodoProps}
+        setUpdate={this.props.setUpdate}
+        />
+        ))}
+      </ul>
+    </React.Fragment>
+    )
+  }
+}
+
+export default TodosList;
